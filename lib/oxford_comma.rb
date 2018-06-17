93244688e0fed0ef2f5d0bcb2884043f42d0d15e
Edsel Caprice <caprice@mail.usf.edu>
2018-06-17 18:38:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,7 @@
 def oxford_comma(array)
-  [array[0...-1].join(", "), array.last].join(", and ")
+  if array.size > 2
+    [array[0...-1].join(", "), array.last].join(", and ")
+  else
+    array.join
 
 end
