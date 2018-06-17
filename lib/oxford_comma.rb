b7ab4719b292713d09a7eff06ccbba79d387bcb5
Edsel Caprice <caprice@mail.usf.edu>
2018-06-17 18:45:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   if array.size > 2
-    [array.join(", "), array.last].join(", and ")
+    [array.join[0...-1](", "), array.last].join(", and ")
 
   elsif array.size == 2
     "#{array[0]} and #{array[1]}"
